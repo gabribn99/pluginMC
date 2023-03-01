2254b99983c2eb4ef0162358f7fe3331bdb0a35e
Gabriel Ballester Niebla <56097204+gabribn99@users.noreply.github.com>
2023-03-01 01:44:03 +0000

    Mensaje de entrada y salida de colores

--- src/main/java/gabriel/events/PlayerJoinQuit.java
+++ src/main/java/gabriel/events/PlayerJoinQuit.java
@@ -1,5 +1,6 @@
 package gabriel.events;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -10,11 +11,11 @@ public class PlayerJoinQuit implements Listener {
     @EventHandler
     public void Entrada(PlayerJoinEvent event) {
 
-        event.setJoinMessage("El jugador de " + event.getPlayer().getName() + " se ha unido");
+        event.setJoinMessage(ChatColor.GREEN + "El jugador " + ChatColor.WHITE + event.getPlayer().getName() + ChatColor.RESET + ChatColor.GREEN + " se ha unido");
     }
 
     @EventHandler
     public void Salida(PlayerQuitEvent event) {
-        event.setQuitMessage("El jugador " + event.getPlayer().getName() + " se ha ido");
+        event.setQuitMessage(ChatColor.RED + "El jugador " + ChatColor.WHITE + event.getPlayer().getName() + ChatColor.RESET + ChatColor.RED + " se ha ido");
     }
 }

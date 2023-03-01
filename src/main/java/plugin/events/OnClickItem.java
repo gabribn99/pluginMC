2d6f1c1ed9e1384cb532b996b8bc3f1b1df82591
Gabriel Ballester Niebla <56097204+gabribn99@users.noreply.github.com>
2023-03-01 17:03:23 +0000

    Try hand

--- src/main/java/plugin/events/OnClickItem.java
+++ src/main/java/plugin/events/OnClickItem.java
@@ -1,18 +1,20 @@
 package plugin.events;
 
+import org.apache.commons.text.WordUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class OnClickItem implements Listener {
-	@EventHandler
+    @EventHandler
     public void Entrada(EntityDamageByEntityEvent event) {
-		 if(event.getDamager() instanceof Player) {
-			Player atacante = (Player) event.getDamager();
-			System.out.println( event.getEntity().toString());
-			atacante.sendMessage("Has golpeado");
-			
-		}
-		}
+        if (event.getDamager() instanceof Player) {
+            Player atacante = (Player) event.getDamager();
+            System.out.println(event.getEntity().toString());
+            String itemInHand = ((Player) event.getDamager()).getInventory().getItemInMainHand().getType().toString();
+            atacante.sendMessage("Has golpeado a " + event.getEntity().getName() + " con " + WordUtils.capitalizeFully(itemInHand.replace("_", " ")));
+
+        }
+    }
 }
